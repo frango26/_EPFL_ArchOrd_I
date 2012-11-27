12f3ce4a78f00fde928e8a00027cc8f4b5976c08
komanda-phanzu <komanda.phanzu@epfl.ch>
2012-11-27 23:05:56 +0100

    count and format is done

--- niosII_simulator/asm/modifyFormat.asm
+++ niosII_simulator/asm/modifyFormat.asm
@@ -1,6 +1,6 @@
 main:
 	addi a0, zero, data				; a0: Data address
-	addi a1, zero, 6				; a1: Number of elements
+	addi a1, zero, 9				; a1: Number of elements
 	call modifyFormat
 	break							; end of the program
 modifyFormat:
@@ -11,10 +11,10 @@ modifyFormat:
 modifyFormat_loop:
 	bgeu t1, a1, modifyFormat_return	; if (  t1 > a1 ) goto return;
 	ldw t2, 0(t0)					; t2 = mem[t0]
-	slli t2, t2, 11					; t2 = t2 << 21;
+;	slli t2, t2, 11					; t2 = t2 << 21;
 ;	srli t2, t2, 11					; t2 = t2 >> 11;
-	srli t2, t2, 21					; t2 = t2 << 21;
-;	andi t2, t2, 0xFFFF				; t2 = t2 & 0xffff ; // set the bit between 31 and 16
+	srli t2, t2, 11					; t2 = t2 << 21;
+	andi t2, t2, 0x7ff				; t2 = t2 & 0xffff ; // set the bit between 11 and 0
 	ori	 t2, t2, 0xF800				; t2 = t2 | 0xf800 ; // set the bit between 15 and 11
 ;	stw  t2, 0(t0)					;
 	stw  t2, 0(t7)					;
@@ -26,9 +26,13 @@ modifyFormat_loop:
 modifyFormat_return:
 	ret								; return to caller
 data:								; data initialization
+	.word 0xFFFFFFFF
 									;              |            |
 									;  3           2            1           0
 									; 1098 7654 3210 9876 5432 1098 7654 3210 
+	.word 0x7FF				        ; 0000 0000 0000 0000 0000 0111 1111 1111
+	.word 0x3FF800				    ; 0000 0000 0011 1111 1111 1000 0000 0000
+
 	.word 0x1FFC00				    ; 0000 0000 0001 1111 1111 1100 0000 0000 => 
 									; 0000 0000 0000 0000 1111 1000 0000 0000
 	.word 0x0				        ; 0000 0000 0000 0000 0000 0000 0000 0000 => 
@@ -36,7 +40,7 @@ data:								; data initialization
 	.word 0xFFFFFFFF			    ; 1111 1111 1111 1111 1111 1111 1111 1111 => 
 									; 0000 0000 0000 0000 1111 1000 0000 0000
 	.word 0xAAEAAFFF				; 1010 1010 1110 1010 1010 1111 1111 1111 => 2AB
-									; 0000 0000 0000 0000 1111 1110 1010 1011 => FAAB
+									; 0000 0000 0000 0000 1111 1110 1010 1011 => FEAB
 	.word 0x6
 	.word 3
 ;	.word 0xAAAAAAAA
